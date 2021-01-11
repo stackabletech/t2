4a10977765ba4f1fb52b0259efa6873bb3ac7ea9
Nikolaus Winter <mail@nikolaus-winter.de>
2021-01-11 12:15:04 +0100

    raw SpringBoot REST App created by spring.io initializr

---
+++ src/main/java/tech/stackable/t2/T2ServerApplication.java
@@ -0,0 +1,13 @@
+package tech.stackable.t2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/stackable/t2/T2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.stackable.t2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
