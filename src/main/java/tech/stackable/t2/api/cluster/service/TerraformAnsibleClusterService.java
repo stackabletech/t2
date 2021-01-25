8bc076a2539f0393fb54c25c48e2dd80caa7ba18
Nikolaus Winter <mail@nikolaus-winter.de>
2021-01-25 19:02:43 +0100

    create/destroy via Terraform (draft)

--- src/main/java/tech/stackable/t2/api/cluster/service/TerraformAnsibleClusterService.java
+++ src/main/java/tech/stackable/t2/api/cluster/service/TerraformAnsibleClusterService.java
@@ -1,15 +1,24 @@
 package tech.stackable.t2.api.cluster.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Repository;
 
 import tech.stackable.t2.api.cluster.domain.Cluster;
+import tech.stackable.t2.terraform.TerraformRunner;
 
 /**
  * Manages 'real' clusters provisioned with Terraform and Ansible
@@ -20,8 +29,17 @@ public class TerraformAnsibleClusterService implements ClusterService {
   
   private static final Logger LOGGER = LoggerFactory.getLogger(TerraformAnsibleClusterService.class);  
 
+  @Autowired
+  @Qualifier("workspaceDirectory")
+  private Path workspaceDirectory;
+  
+  @Autowired
+  ResourceLoader resourceLoader;  
+  
   private int provisionClusterLimit = -1;  
   
+  private Map<UUID, Cluster> clusters = new HashMap<>();
+
   public TerraformAnsibleClusterService(@Value("${t2.feature.provision-cluster-limit}") int provisionClusterLimit) {
     this.provisionClusterLimit = provisionClusterLimit;
     LOGGER.info("ClusterService cluster number limit: {}", this.provisionClusterLimit);
@@ -29,22 +47,68 @@ public class TerraformAnsibleClusterService implements ClusterService {
 
   @Override
   public Cluster createCluster() {
-    throw new UnsupportedOperationException("wip");
+    // TODO set state
+    synchronized(this.clusters) {
+      if(clusters.size()>=this.provisionClusterLimit) {
+        throw new ClusterLimitReachedException();
+      }
+      Cluster cluster = new Cluster();
+      clusters.put(cluster.getId(), cluster);
+      
+      try {
+        Files.createDirectory(workspaceDirectory.resolve(cluster.getId().toString()));
+      } catch (IOException ioe) {
+        LOGGER.error("Workspace directory for cluster {} could not be created.", cluster.getId(), ioe);
+        throw new RuntimeException(String.format("Workspace directory for cluster %s could not be created.", cluster.getId()));
+      }
+
+      try {
+        Files.createDirectory(workspaceDirectory.resolve(cluster.getId().toString()).resolve("terraform"));
+        Files.copy(this.resourceLoader.getResource("classpath:templates/terraform/cluster.tf").getFile().toPath(), workspaceDirectory.resolve(cluster.getId().toString()).resolve("terraform").resolve("cluster.tf"));
+      } catch (IOException ioe) {
+        LOGGER.error("Terraform directory for cluster {} could not be created.", cluster.getId(), ioe);
+        throw new RuntimeException(String.format("Terraform directory for cluster %s could not be created.", cluster.getId()));
+      }
+      
+      new Thread(() -> {
+        TerraformRunner runner = TerraformRunner.create(workspaceDirectory.resolve(cluster.getId().toString()).resolve("terraform").resolve("cluster.tf"), String.format("t2-%s", cluster.getId()));
+        runner.init();
+        runner.plan();
+        runner.apply();
+      }).start();
+      
+      return cluster;
+    }
   }
 
   @Override
   public Collection<Cluster> getAllClusters() {
-    throw new UnsupportedOperationException("wip");
+    return this.clusters.values();
   }
 
   @Override
   public Cluster getCluster(UUID id) {
-    throw new UnsupportedOperationException("wip");
+    return this.clusters.get(id);
   }
 
   @Override
   public void deleteCluster(UUID id) {
-    throw new UnsupportedOperationException("wip");
+    // TODO set state
+    synchronized(this.clusters) {
+      Cluster cluster = this.clusters.get(id);
+      if(cluster==null) {
+        return;
+      }
+      
+      // TODO check if dir exists etc.
+      
+      new Thread(() -> {
+        TerraformRunner runner = TerraformRunner.create(workspaceDirectory.resolve(cluster.getId().toString()).resolve("terraform").resolve("cluster.tf"), String.format("t2-%s", cluster.getId()));
+        runner.destroy();
+      }).start();
+      
+      this.clusters.remove(id);
+    }
   }
 
 }
---
+++ src/main/resources/templates/terraform/cluster.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    profitbricks = {
+      source = "ionos-cloud/profitbricks"
+      version = "1.6.5"
+    }
+  }
+}
+
+variable "ionos_username" {
+  description = "Username to be used with the Profitbricks Cloud Provider - set using environment variable TF_VAR_ionos_username"
+  type        = string
+  sensitive   = true
+}
+
+variable "ionos_password" {
+  description = "Password to be used with the Profitbricks Cloud Provider - set using environment variable TF_VAR_ionos_password"
+  type        = string
+  sensitive   = true
+}
+
+variable "ionos_datacenter" {
+  description = "Username to be used with the Profitbricks Cloud Provider - set using environment variable TF_VAR_ionos_datacenter"
+  type        = string
+  sensitive   = true
+}
+
+provider "profitbricks" {
+  username = var.ionos_username
+  password = var.ionos_password
+}
+
+resource "profitbricks_datacenter" "datacenter" {
+  name = var.ionos_datacenter
+  location = "de/fra"
+  description = "Provisioned via terraform by T2"
+}
+
+data "profitbricks_image" "centos7" {
+  name     = "CentOS"
+  type     = "HDD"
+  version  = "7"
+  location = "de/fra"
+}
+
+// Internet facing lan
+resource "profitbricks_lan" "external" {
+  name = "External Network"
+  datacenter_id = profitbricks_datacenter.datacenter.id
+  public = true
+}
+
+# NAT Server on the Edge to forward requests and serve as VPN endpoint
+resource "profitbricks_server" "nat" {
+  name = "nat"
+  datacenter_id = profitbricks_datacenter.datacenter.id
+  cores = 2
+  ram = 1024
+  availability_zone = "ZONE_1"
+
+  image_name = data.profitbricks_image.centos7.name
+  image_password = "nureintest"
+
+  volume {
+    name = "nat-storage"
+
+    size = 15
+    disk_type = "SSD"
+  }
+
+  nic {
+    lan = profitbricks_lan.external.id
+    dhcp = true
+    firewall_active = false
+  }
+}
+
