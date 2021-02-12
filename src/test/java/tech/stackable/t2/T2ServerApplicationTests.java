608cb67aff81223f5abd2fb9f1a07b68f3e3d814
Nikolaus Winter <mail@nikolaus-winter.de>
2021-02-12 12:49:31 +0100

    disabled test that launches SpringBoot applicaton as there is no valid config

--- src/test/java/tech/stackable/t2/T2ServerApplicationTests.java
+++ src/test/java/tech/stackable/t2/T2ServerApplicationTests.java
@@ -1,8 +1,10 @@
 package tech.stackable.t2;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class T2ServerApplicationTests {
 
