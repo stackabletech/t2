9bfab8963e8c469dabc36d7a68725b273286eaef
Nikolaus Winter <mail@nikolaus-winter.de>
2021-05-11 10:20:15 +0200

    Add Stackable agent version to versions sheet

--- templates/demo-centos-7/files/roles/operators/files/collect-stackable-versions.sh
+++ templates/demo-centos-7/files/roles/operators/files/collect-stackable-versions.sh
@@ -2,10 +2,4 @@
 
 printf "Node $HOSTNAME\n\n" > /tmp/stackable-versions.txt
 
-printf "Python version: " >> /tmp/stackable-versions.txt
-python --version &>> /tmp/stackable-versions.txt
-
-printf "Python3 version: " >> /tmp/stackable-versions.txt
-python3 --version &>> /tmp/stackable-versions.txt
-
-printf "\n\n" >> /tmp/stackable-versions.txt
+printf "No version information available.\n\n" >> /tmp/stackable-versions.txt
