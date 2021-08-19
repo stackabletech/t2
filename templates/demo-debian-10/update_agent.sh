e3890ac96ef0ecdccb5f08717f6bbfb74eed7dd1
Nikolaus Winter <mail@nikolaus-winter.de>
2021-08-19 22:17:07 +0200

    update scripts: refresh package versions before showing options to user (#121)

--- templates/demo-debian-10/update_agent.sh
+++ templates/demo-debian-10/update_agent.sh
@@ -3,6 +3,8 @@
 echo
 echo "Reading installed version for stackable-agent..."
 echo
+sh resources/stackable.sh orchestrator "apt update"
+echo
 sh resources/stackable.sh nodes "apt list -a stackable-agent 2> /dev/null" | grep stackable
 echo
 read -p "Which version do you want to update to? " version
