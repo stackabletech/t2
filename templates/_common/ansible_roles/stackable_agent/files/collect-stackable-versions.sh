b24b0766051d2fa1388a055c098cd87c64b2cfb8
Nikolaus Winter <mail@nikolaus-winter.de>
2021-08-23 15:37:52 +0200

    re-activated and fixed version info file generation

--- templates/_common/ansible_roles/stackable_agent/files/collect-stackable-versions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-printf "Node $HOSTNAME\n\n" > /tmp/stackable-versions.txt
-
-#/opt/stackable-agent/stackable-agent --version &>> /tmp/stackable-versions.txt
-
-printf "\n\n" >> /tmp/stackable-versions.txt
