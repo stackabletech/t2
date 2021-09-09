bacdcbbbb6383f75c31d702a686bcc40bddebae8
Nikolaus Winter <mail@nikolaus-winter.de>
2021-09-09 16:44:02 +0200

    renamed update scripts already as they can be tested in the end...

--- templates/demo-centos-7/update_operator.sh
+++ templates/demo-centos-7/update_operator.sh
@@ -5,20 +5,20 @@ echo "List of available operators"
 echo
 sh resources/stackable.sh orchestrator "yum clean all 1>/dev/null 2>&1"
 { 
-    echo "`sh resources/stackable.sh orchestrator "yum list installed 2> /dev/null | grep 'stackable'" | grep 'operator-server' | sed 's/stackable-//' | sed 's/-operator-server.x86_64.*//'`" ;
-    echo "`sh resources/stackable.sh orchestrator "yum list available 2> /dev/null | grep 'stackable'" | grep 'operator-server' | sed 's/stackable-//' | sed 's/-operator-server.x86_64.*//'`" ; 
+    echo "`sh resources/stackable.sh orchestrator "yum list installed 2> /dev/null | grep 'stackable'" | grep 'operator' | sed 's/stackable-//' | sed 's/-operator.x86_64.*//'`" ;
+    echo "`sh resources/stackable.sh orchestrator "yum list available 2> /dev/null | grep 'stackable'" | grep 'operator' | sed 's/stackable-//' | sed 's/-operator.x86_64.*//'`" ; 
 } | sort -u
 echo
 
 read -p "Which operator do you want to update? " operator
 
 echo
-echo "Reading installed version for stackable-$operator-operator-server.x86_64..."
+echo "Reading installed version for stackable-$operator-operator.x86_64..."
 echo
-sh resources/stackable.sh orchestrator "yum list installed stackable-$operator-operator-server.x86_64 2> /dev/null"
+sh resources/stackable.sh orchestrator "yum list installed stackable-$operator-operator.x86_64 2> /dev/null"
 echo
 echo "Available versions: "
-sh resources/stackable.sh orchestrator "yum list available --showduplicates 2> /dev/null | grep 'stackable-$operator-operator-server.x86_64'" | awk '{print $2}' | sort -u
+sh resources/stackable.sh orchestrator "yum list available --showduplicates 2> /dev/null | grep 'stackable-$operator-operator.x86_64'" | awk '{print $2}' | sort -u
 echo
 
 read -p "Which version do you want to update to? " version
