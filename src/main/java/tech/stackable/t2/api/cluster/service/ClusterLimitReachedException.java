adb22d9b9a6e9b80b04e75fb4309503efd4fa5ee
Nikolaus Winter <mail@nikolaus-winter.de>
2021-10-19 10:00:58 +0200

    removed cluster-limit feature (should be ensured by cloud provider account)

--- src/main/java/tech/stackable/t2/api/cluster/service/ClusterLimitReachedException.java
+++
@@ -1,9 +0,0 @@
-package tech.stackable.t2.api.cluster.service;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@SuppressWarnings("serial")
-@ResponseStatus(code = HttpStatus.CONFLICT, reason = "T2 has reached its limit of coexisting clusters.")
-public class ClusterLimitReachedException extends RuntimeException {
-}
