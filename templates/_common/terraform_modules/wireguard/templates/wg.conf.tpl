35f5bad45cb4c083985a8ecc8cf1d29c781a5445
Nikolaus Winter <mail@nikolaus-winter.de>
2021-10-26 20:15:36 +0200

    rename nat/bastion_host -> edge (draft)

--- templates/_common/terraform_modules/wireguard/templates/wg.conf.tpl
+++ templates/_common/terraform_modules/wireguard/templates/wg.conf.tpl
@@ -1,7 +1,7 @@
 [Interface]
 Address = 10.11.12.1/24
 ListenPort = 52888
-PrivateKey = ${wg_nat_private_key}
+PrivateKey = ${wg_host_private_key}
 
 %{ for index, public_key in wg_client_public_keys ~}
 [Peer]
--- templates/_common/terraform_modules/wireguard/wireguard.tf
+++ templates/_common/terraform_modules/wireguard/wireguard.tf
@@ -19,12 +19,12 @@ variable "endpoint_ip" {
 }
 
 # wireguard configfile for edge node
-resource "local_file" "wireguard_nat_config" {
+resource "local_file" "wireguard_host_config" {
   filename = var.server_config_filename
   file_permission = "0440"
   content = templatefile("${path.module}/templates/wg.conf.tpl",
     {
-      wg_nat_private_key = yamldecode(file("wireguard.yaml"))["server"]["privateKey"]
+      wg_host_private_key = yamldecode(file("wireguard.yaml"))["server"]["privateKey"]
       wg_client_public_keys = [ for client in yamldecode(file("wireguard.yaml"))["clients"]: client.publicKey ]
     }
   )
