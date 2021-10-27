6333dc76d44d9fe4cd1b444263f53ea74d9b3f62
Nikolaus Winter <mail@nikolaus-winter.de>
2021-10-27 12:04:59 +0200

    extracted creation of SSH keys as Ansible vars to common Terraform module

--- templates/_common/terraform_modules/ionos_inventory/ionos_inventory.tf
+++ templates/_common/terraform_modules/ionos_inventory/ionos_inventory.tf
@@ -56,3 +56,7 @@ resource "local_file" "ansible-inventory" {
   file_permission = "0440"
 } 
 
+module "ansible_variables_public_ssh_keys" {
+  source                        = "../common_ansible_variables_public_ssh_keys"
+}
+
