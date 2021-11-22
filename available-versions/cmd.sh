e84f3fd367323f556c8dab01acee610a19f5dfb4
Nikolaus Winter <mail@nikolaus-winter.de>
2021-11-22 22:32:04 +0100

    available versions (draft)

--- available-versions/cmd.sh
+++ available-versions/cmd.sh
@@ -1,2 +1,1 @@
-yum list available --showduplicates 2> /dev/null | grep 'stackable-agent.x86_64' | sed 's/.x86_64//' | sed 's/.el8//' | awk '{print $1"/"$2}' | sed 's/-0./-/' | sed 's/-0//' | sort -u
-yum list available --showduplicates 2> /dev/null | grep 'stackable' | grep 'operator' | sed 's/.x86_64//' | sed 's/.el8//' | awk '{print $1"/"$2}' | sed 's/-0./-/' | sed 's/-0//' | sort -u
+helm search repo --versions --devel
