f56870697bb44f53abcfd7310fa31e1ee58f0ef2
Nikolaus Winter <mail@nikolaus-winter.de>
2022-01-05 15:47:08 +0100

    fixed name of Hetzner CentOS 8 image

--- templates/_common/terraform_modules/hcloud/hcloud.tf
+++ templates/_common/terraform_modules/hcloud/hcloud.tf
@@ -17,7 +17,7 @@ variable "cluster_name" {
 }
 
 variable "os_image" {
-  description = "Image of the OS for the node, e.g. centos-8"
+  description = "Image of the OS for the node, e.g. centos-stream-8"
   type        = string
 }
 
