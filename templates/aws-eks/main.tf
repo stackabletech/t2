1d941bda677dbc85c2776278dc42abb9f6a98877
Nikolaus Winter <mail@nikolaus-winter.de>
2022-01-24 17:06:49 +0100

    working base version of AWS EKS template

--- templates/aws-eks/main.tf
+++ templates/aws-eks/main.tf
@@ -1,4 +1,31 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.20.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
+  }
+
   required_version = ">= 0.15, < 2.0.0"
 }
 
@@ -29,13 +56,13 @@ data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.66.0"
+  version = "3.2.0"
 
   name                 = "${var.cluster_name}-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.11.0/24", "10.0.22.0/24", "10.0.33.0/24"]
-  public_subnets       = ["10.0.44.0/24", "10.0.55.0/24", "10.0.66.0/24"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
@@ -70,10 +97,43 @@ resource "aws_security_group" "worker_group_mgmt_one" {
   }
 }
 
+resource "aws_security_group" "worker_group_mgmt_two" {
+  name_prefix = "worker_group_mgmt_two"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "17.24.0"
   cluster_name    = var.cluster_name
-  cluster_version = "1.20"
+  cluster_version = "1.21"
   subnets         = module.vpc.private_subnets
 
   vpc_id = module.vpc.vpc_id
@@ -82,19 +142,21 @@ module "eks" {
     root_volume_type = "gp2"
   }
 
-cluster_endpoint_private_access = "true"
-  cluster_endpoint_public_access  = "true"
-
-  write_kubeconfig      = true
-  manage_aws_auth       = true
-
   worker_groups = [
     {
       name                          = "worker-group-1"
       instance_type                 = "t2.small"
-      asg_desired_capacity          = can(yamldecode(file("cluster.yaml"))["spec"]["node_count"]) ? yamldecode(file("cluster.yaml"))["spec"]["node_count"] : 3
+      additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-    }
+      asg_desired_capacity          = can(yamldecode(file("cluster.yaml"))["spec"]["node_count"]) ? yamldecode(file("cluster.yaml"))["spec"]["node_count"] : 3
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
   ]
 }
 
@@ -104,7 +166,7 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
-}  
+}
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
@@ -118,3 +180,8 @@ resource "local_file" "kubeconfig" {
   content = module.eks.kubeconfig
   file_permission = "0400"
 } 
+
+# create file w/ stackable component versions for Ansible inventory
+module "stackable_component_versions" {
+  source = "./terraform_modules/stackable_component_versions"
+}
