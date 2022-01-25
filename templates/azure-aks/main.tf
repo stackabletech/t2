fb65c6e68ea7ec708620ee7e47ddf060408c9c26
Nikolaus Winter <mail@nikolaus-winter.de>
2022-01-25 22:26:14 +0100

    updated software and Terraform providers in T2 (except AWS EKS)

--- templates/azure-aks/main.tf
+++ templates/azure-aks/main.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "=2.93.1"
     }
   }
 }
--- templates/pluscloud-open-centos-8/main.tf
+++ templates/pluscloud-open-centos-8/main.tf
@@ -17,7 +17,7 @@ terraform {
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "1.43.0"
+      version = "1.46.0"
     }
   }
 }
