7555eac28c9d6404494798675952aa7c488cb3b3
Nikolaus Winter <mail@nikolaus-winter.de>
2022-08-31 18:02:35 +0200

    added Hetzner Cloud Debian 10/11

---
+++ templates/hcloud-debian-11/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 0.15, < 2.0.0"
+}
+
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.35.1"
+    }
+  }
+}
+
+variable "hcloud_token" {
+  description = "Token to be used with Hetzner cloud"
+  type        = string
+  sensitive   = true
+}
+
+variable "cluster_name" {
+  description = "Name of the cluster, used as a prefix on the names of the resources created here"
+  type        = string
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+module "stackable_component_versions" {
+  source = "./terraform_modules/stackable_component_versions"
+}
+
+module "hcloud" {
+  source                     = "./terraform_modules/hcloud"
+  cluster_name               = var.cluster_name
+  os_image                   = "debian-11"
+}
