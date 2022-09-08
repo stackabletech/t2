24358a815637c532aed1d4b333d543ed19b1322c
Nikolaus Winter <mail@nikolaus-winter.de>
2022-09-08 11:46:16 +0200

    HCloud servers: switched to CPX servers due to availability issues

--- templates/_common/terraform_modules/hcloud/hcloud.tf
+++ templates/_common/terraform_modules/hcloud/hcloud.tf
@@ -28,7 +28,7 @@ locals {
     for type, definition in yamldecode(file("cluster.yaml"))["spec"]["nodes"] : [
       for i in range(1, definition.numberOfNodes + 1): {
         name = "${type}-${i}" 
-        serverType = can(definition.serverType) ? definition.serverType : "cx21"
+        serverType = can(definition.serverType) ? definition.serverType : "cpx21"
         k8s_node = can(definition.k8s_node) ? definition.k8s_node : true
       }
     ]
