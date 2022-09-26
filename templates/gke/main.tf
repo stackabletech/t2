fae9775f0f673507a9788ebc0e475ec987fee842
Nikolaus Winter <mail@nikolaus-winter.de>
2022-09-26 15:14:56 +0200

    GKE: template w/ variables, ready to be tested in T2-dev

--- templates/gke/main.tf
+++ templates/gke/main.tf
@@ -17,18 +17,26 @@ variable "cluster_name" {
 }
 
 variable "google_cloud_project_id" {
-  description = "Project ID in Google Cloud"
+  description = "Project ID in Google Cloud in which the cluster is to be created"
+  type        = string
 }
 
-# TODO: get from cluster.yaml
 locals {
-  region = "us-east4"
-  zone = "us-east4-a"
+  # region from cluster definition file
+  region = can(yamldecode(file("cluster.yaml"))["spec"]["region"]) ? yamldecode(file("cluster.yaml"))["spec"]["region"] : "europe-central2"
 }
 
 provider "google" {
   project = var.google_cloud_project_id
   region  = local.region
+}
+
+# Determine zones which can be used for the cluster
+data "google_compute_zones" "available" {
+}
+
+locals {
+  zone = data.google_compute_zones.available.names[0]
 }
 
 # VPC
@@ -49,11 +57,20 @@ resource "google_compute_subnetwork" "subnet" {
 resource "google_container_cluster" "cluster" {
   name     = "${var.cluster_name}"
   location = local.zone
-  
-  initial_node_count       = can(yamldecode(file("cluster.yaml"))["spec"]["node_count"]) ? yamldecode(file("cluster.yaml"))["spec"]["node_count"] : 3
-
+  initial_node_count = can(yamldecode(file("cluster.yaml"))["spec"]["node_count"]) ? yamldecode(file("cluster.yaml"))["spec"]["node_count"] : 3
+  min_master_version = can(yamldecode(file("cluster.yaml"))["spec"]["k8sVersion"]) ? yamldecode(file("cluster.yaml"))["spec"]["k8sVersion"] : null
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
+  node_config {
+    machine_type = can(yamldecode(file("cluster.yaml"))["spec"]["machineType"]) ? yamldecode(file("cluster.yaml"))["spec"]["machineType"] : "e2-standard-2"
+  }
+  private_cluster_config {
+    enable_private_nodes = true
+    enable_private_endpoint = false 
+    master_ipv4_cidr_block = "10.5.0.0/28"
+  }
+  ip_allocation_policy {
+  }
 }
 
 # Create kubeconfig
