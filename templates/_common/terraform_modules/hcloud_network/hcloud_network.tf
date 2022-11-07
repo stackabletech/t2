d42b4de9dc114c6f9136fc52506c5127c41fe72f
Nikolaus Winter <mail@nikolaus-winter.de>
2022-11-07 16:40:58 +0100

    labels in HCloud clusters

--- templates/_common/terraform_modules/hcloud_network/hcloud_network.tf
+++ templates/_common/terraform_modules/hcloud_network/hcloud_network.tf
@@ -18,16 +18,21 @@ variable "cluster_name" {
   description = "name of the cluster"
 }
 
+variable "labels" {
+  description = "custom labels for the network resources"
+}
+
 resource "hcloud_network" "network" {
-  name     = "${var.cluster_name}-network"
-  ip_range = "10.0.0.0/16"
+  name            = "${var.cluster_name}-network"
+  ip_range        = "10.0.0.0/16"
+  labels          = var.labels
 }
 
 resource "hcloud_network_subnet" "subnet" {
-  type         = "cloud"
-  network_id   = hcloud_network.network.id
-  network_zone = "eu-central"
-  ip_range     = "10.0.1.0/24"
+  type            = "cloud"
+  network_id      = hcloud_network.network.id
+  network_zone    = "eu-central"
+  ip_range        = "10.0.1.0/24"
 }
 
 output "network" {
